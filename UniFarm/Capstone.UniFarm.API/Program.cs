4e6660c785fb21cf4ef1fa676eb5ffed924d652c
HuuTri130401 <trith130401@gmail.com>
2024-01-24 15:09:08 +0700

    Update file Program.cs and Deploy to Azure

--- UniFarm/Capstone.UniFarm.API/Program.cs
+++ UniFarm/Capstone.UniFarm.API/Program.cs
@@ -44,20 +44,41 @@ try
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
+    //============Configure CORS============//
+    builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+                   .AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+    });
+
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    //if (app.Environment.IsDevelopment())
+    //{
+    //    app.UseSwagger();
+    //    app.UseSwaggerUI(c =>
+    //    {
+    //        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name v1");
+    //        c.RoutePrefix = string.Empty;
+    //    });
+    //}
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseHttpsRedirection();
 
     app.UseAuthorization();
 
     app.MapControllers();
+
+    app.UseCors();
 
     app.Run();
 
