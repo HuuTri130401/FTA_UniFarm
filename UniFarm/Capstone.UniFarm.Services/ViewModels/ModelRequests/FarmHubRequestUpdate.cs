0d68a977ff54aa95cf4f07c17577170f5ad833b9
HuuTri130401 <trith130401@gmail.com>
2024-02-22 22:20:07 +0700

    Add API for Manage FarmHub

--- UniFarm/Capstone.UniFarm.Services/ViewModels/ModelRequests/FarmHubRequestUpdate.cs
+++ UniFarm/Capstone.UniFarm.Services/ViewModels/ModelRequests/FarmHubRequestUpdate.cs
@@ -6,14 +6,13 @@ using System.Threading.Tasks;
 
 namespace Capstone.UniFarm.Services.ViewModels.ModelRequests
 {
-    public class FarmHubRequest
+    public class FarmHubRequestUpdate
     {
         public string Name { get; set; }
         public string Code { get; set; }
         public string? Description { get; set; }
         public string? Image { get; set; }
         public string Address { get; set; }
-        public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public string Status { get; set; }
     }
