80e3acad67f3265e98d8abd8f5ecae12a97d4e63
227373 - Nguyễn Đình Tiến <tientung2029901@gmail.com>
2024-04-07 01:28:38 +0700

    Update OrderDetail model

---
+++ UniFarm/Capstone.UniFarm.Domain/Migrations/20240406182755_UpdateOrderDetail.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Capstone.UniFarm.Domain.Migrations
+{
+    public partial class UpdateOrderDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginUnitPrice",
+                table: "OrderDetail");
+
+            migrationBuilder.DropColumn(
+                name: "TotalOriginPrice",
+                table: "OrderDetail");
+
+            migrationBuilder.DropColumn(
+                name: "TotalFarmHubPrice",
+                table: "Order");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "OriginUnitPrice",
+                table: "OrderDetail",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalOriginPrice",
+                table: "OrderDetail",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalFarmHubPrice",
+                table: "Order",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+    }
+}
