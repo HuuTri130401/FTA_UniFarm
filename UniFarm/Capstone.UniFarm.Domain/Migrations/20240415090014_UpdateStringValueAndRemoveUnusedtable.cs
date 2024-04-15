4f4d2a25848f208016c77eacd46932ef04661118
227373 - Nguyễn Đình Tiến <tientung2029901@gmail.com>
2024-04-15 16:32:54 +0700

    update model

---
+++ UniFarm/Capstone.UniFarm.Domain/Migrations/20240415090014_UpdateStringValueAndRemoveUnusedtable.cs
@@ -0,0 +1,411 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Capstone.UniFarm.Domain.Migrations
+{
+    public partial class UpdateStringValueAndRemoveUnusedtable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoleClaims_Roles_RoleId",
+                table: "RoleClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserClaims_Account_UserId",
+                table: "UserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTokens_Account_UserId",
+                table: "UserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserTokens",
+                table: "UserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserClaims",
+                table: "UserClaims");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RoleClaims",
+                table: "RoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "OrderDetail");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "TotalBenefit",
+                table: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "UserTokens",
+                newName: "AspNetUserTokens");
+
+            migrationBuilder.RenameTable(
+                name: "UserClaims",
+                newName: "AspNetUserClaims");
+
+            migrationBuilder.RenameTable(
+                name: "RoleClaims",
+                newName: "AspNetRoleClaims");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserClaims_UserId",
+                table: "AspNetUserClaims",
+                newName: "IX_AspNetUserClaims_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                newName: "IX_AspNetRoleClaims_RoleId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "PriceTable",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Payment",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "Payment",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Payment",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankOwnerName",
+                table: "Payment",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankName",
+                table: "Payment",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankAccountNumber",
+                table: "Payment",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Order",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Order",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetUserTokens",
+                table: "AspNetUserTokens",
+                columns: new[] { "UserId", "LoginProvider", "Name" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetUserClaims",
+                table: "AspNetUserClaims",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetRoleClaims",
+                table: "AspNetRoleClaims",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetRoleClaims_Roles_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_Account_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_Account_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetRoleClaims_Roles_RoleId",
+                table: "AspNetRoleClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_Account_UserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_Account_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetUserTokens",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetUserClaims",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetRoleClaims",
+                table: "AspNetRoleClaims");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserTokens",
+                newName: "UserTokens");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserClaims",
+                newName: "UserClaims");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetRoleClaims",
+                newName: "RoleClaims");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "UserClaims",
+                newName: "IX_UserClaims_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "RoleClaims",
+                newName: "IX_RoleClaims_RoleId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "PriceTable",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Payment",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "Payment",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Payment",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankOwnerName",
+                table: "Payment",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankName",
+                table: "Payment",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankAccountNumber",
+                table: "Payment",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "OrderDetail",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Order",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalBenefit",
+                table: "Order",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Category",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserTokens",
+                table: "UserTokens",
+                columns: new[] { "UserId", "LoginProvider", "Name" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserClaims",
+                table: "UserClaims",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RoleClaims",
+                table: "RoleClaims",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoleClaims_Roles_RoleId",
+                table: "RoleClaims",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserClaims_Account_UserId",
+                table: "UserClaims",
+                column: "UserId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTokens_Account_UserId",
+                table: "UserTokens",
+                column: "UserId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ UniFarm/Capstone.UniFarm.Domain/Migrations/20240415090828_RollbackUserClaims.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Capstone.UniFarm.Domain.Migrations
+{
+    public partial class RollbackUserClaims : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetRoleClaims_Roles_RoleId",
+                table: "AspNetRoleClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_Account_UserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_Account_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetUserTokens",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetUserClaims",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetRoleClaims",
+                table: "AspNetRoleClaims");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserTokens",
+                newName: "UserTokens");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserClaims",
+                newName: "UserClaims");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetRoleClaims",
+                newName: "RoleClaims");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "UserClaims",
+                newName: "IX_UserClaims_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "RoleClaims",
+                newName: "IX_RoleClaims_RoleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserTokens",
+                table: "UserTokens",
+                columns: new[] { "UserId", "LoginProvider", "Name" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserClaims",
+                table: "UserClaims",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RoleClaims",
+                table: "RoleClaims",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoleClaims_Roles_RoleId",
+                table: "RoleClaims",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserClaims_Account_UserId",
+                table: "UserClaims",
+                column: "UserId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTokens_Account_UserId",
+                table: "UserTokens",
+                column: "UserId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoleClaims_Roles_RoleId",
+                table: "RoleClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserClaims_Account_UserId",
+                table: "UserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTokens_Account_UserId",
+                table: "UserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserTokens",
+                table: "UserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserClaims",
+                table: "UserClaims");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RoleClaims",
+                table: "RoleClaims");
+
+            migrationBuilder.RenameTable(
+                name: "UserTokens",
+                newName: "AspNetUserTokens");
+
+            migrationBuilder.RenameTable(
+                name: "UserClaims",
+                newName: "AspNetUserClaims");
+
+            migrationBuilder.RenameTable(
+                name: "RoleClaims",
+                newName: "AspNetRoleClaims");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserClaims_UserId",
+                table: "AspNetUserClaims",
+                newName: "IX_AspNetUserClaims_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                newName: "IX_AspNetRoleClaims_RoleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetUserTokens",
+                table: "AspNetUserTokens",
+                columns: new[] { "UserId", "LoginProvider", "Name" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetUserClaims",
+                table: "AspNetUserClaims",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetRoleClaims",
+                table: "AspNetRoleClaims",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetRoleClaims_Roles_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_Account_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_Account_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
